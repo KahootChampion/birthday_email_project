135a59858aec04d8e6c06df51677d6a786743117
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2021-06-22 23:38:33 -0600

    Modified birthday_email.py so that variables are organized in a more logically structured manner and the email is organized in a more coherent manner

--- birthday_email.py
+++ birthday_email.py
@@ -3,21 +3,35 @@ import os
 
 sender_email = os.environ.get('SENDER_EMAIL')
 sender_password = os.environ.get('SENDER_PASSWORD')
+
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(sender_email, sender_password)
+
 receiver_email = os.environ.get('RECEIVER_EMAIL')
 receiver_name = os.environ.get('RECEIVER_NAME')
 
-subject = "Generic Title of Email"
+subject = "In commemoration of turning 20 years old!"
+greeter = "Congratulations!"
+message_body = "This is a bot created to wish you a happy birthday as I am pretty terrible with remembering myself...."
+closing_statement = "Thank you"
 
 message = f"""From: Commemoration Bot 
 To: {receiver_name} {receiver_email}
 MIME-Version: 1.0
 Content-type: text/html
 Subject: {subject}
-
-Hello, this is a python bot. Beep Boop!
+{greeter}
+<br>
+<br>
+{message_body}
+<br>
+<br>
+{closing_statement},
+<br>
+<br>
+Haris M.
 """
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(sender_email, sender_password)
 server.sendmail(sender_email,receiver_email,message)
