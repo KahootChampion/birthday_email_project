97c3faa2d50463e0ddb87292b3293fb226e354e7
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2021-06-22 23:13:54 -0600

    Fixed minor bug in main.py, made it so that age is only calculated if the list is not empty

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ today = str(today_datetime)[5:]
 
 df = pd.read_excel('Birthday_Database.xlsx', sheet_name="Sheet1")
 email_list = df[df["Birthday"].astype(str).str[5:] == today].copy()
-email_list['Age'] = today_datetime.year - int(email_list['Birthday'].astype(str).str[0:4])
 
 if email_list.empty:
     print("No one to email")
     exit()
 else:
+    email_list['Age'] = today_datetime.year - int(email_list['Birthday'].astype(str).str[0:4])
     print(email_list)
+
 
 print("\n\n\n")
 
