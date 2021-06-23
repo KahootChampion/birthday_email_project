81b4b04c20f186e27601857669fb951889cd2f9a
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2021-06-22 20:48:10 -0600

    Filtered down the database to produce a list of people who need to be emailed based on wehther or not today is their birthday in main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
+from datetime import date
+
+today = date.today()
+today = str(today)[5:]
+
 df = pd.read_excel('Birthday_Database.xlsx', sheet_name="Sheet1")
-print(df)
+email_list = df[df["Birthday"].astype(str).str[5:] == today]
+
+
+
+if email_list.empty:
+    print("No one to email")
+    exit()
+else:
+    print(email_list)
