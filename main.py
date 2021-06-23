f0cb1fb826ea052dcc7f8107d9c134892664eb38
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2021-06-22 22:59:00 -0600

    Completed age calculation in main.py

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import pandas as pd
 
 from datetime import date
 
-today = date.today()
-today = str(today)[5:]
+
+today_datetime = date.today()
+today = str(today_datetime)[5:]
 
 df = pd.read_excel('Birthday_Database.xlsx', sheet_name="Sheet1")
-email_list = df[df["Birthday"].astype(str).str[5:] == today]
-
-
+email_list = df[df["Birthday"].astype(str).str[5:] == today].copy()
+email_list['Age'] = today_datetime.year - int(email_list['Birthday'].astype(str).str[0:4])
 
 if email_list.empty:
     print("No one to email")
     exit()
 else:
     print(email_list)
+
+print("\n\n\n")
+
+
